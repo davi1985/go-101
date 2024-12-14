01ea7e8c9a79b39cecae07ef4165dc88b6f6904a
Davi Silva <davisilvaphoto@gmail.com>
2024-12-14 15:23:56 -0300

    functions in go

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-import "fmt"
-
-func main() {
-	sayHello("Davi Silva")
-	fmt.Println("Total:", sum(1, 2))
-}
+import (
+	"fmt"
+	"strconv"
+)
 
 func sum(a, b int) int {
 	return a + b
@@ -13,4 +11,28 @@ func sum(a, b int) int {
 
 func sayHello(name string) {
 	fmt.Println(name)
+}
+
+func convertAndSum(a int, b string) (total int, err error) {
+	i, err := strconv.Atoi(b)
+
+	if err != nil {
+		return
+	}
+
+	total = a + i
+	return
+}
+
+func main() {
+	sayHello("Davi Silva")
+	fmt.Println("Total:", sum(1, 2))
+
+	total, err := convertAndSum(1, "5")
+
+	if err != nil {
+		fmt.Println((err))
+	}
+
+	fmt.Println("Total:", total)
 }
