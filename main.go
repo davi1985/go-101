d1962437ded6787362d2eb93064089378a3a797a
Davi Silva <davisilvaphoto@gmail.com>
2024-12-14 15:47:15 -0300

    for traditional statement

--- main.go
+++ main.go
@@ -2,26 +2,8 @@ package main
 
 import "fmt"
 
-var (
-	heads, tails int
-)
-
-func throwCoin(side string) {
-	switch side {
-	case "heads":
-		heads++
-	case "tails":
-		tails++
-
-	default:
-		fmt.Println("try again")
-	}
-}
-
 func main() {
-
-	throwCoin("heads")
-
-	fmt.Println(heads)
-	fmt.Println(tails)
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
 }
