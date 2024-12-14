2c3f540f39486308a7d7397e84aacac44ca1954f
Davi Silva <davisilvaphoto@gmail.com>
2024-12-14 16:35:04 -0300

    maps

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	names := make([]string, 0)
-	ages := make([]int, 10, 20)
+	ages := make(map[string]uint8)
+	ages["davi"] = 39
+	ages["joelma"] = 34
+	ages["laura"] = 2
 
-	fmt.Println(names)
 	fmt.Println(ages)
+
+	val, ok := ages["davi"]
+	fmt.Println(val, ok)
+	value, has := ages["bony"]
+	fmt.Println(value, has)
 }
