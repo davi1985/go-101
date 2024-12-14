0a420c3e1b91655a16f81fbaf7c7a56827a68b0f
Davi Silva <davisilvaphoto@gmail.com>
2024-12-14 14:57:52 -0300

    functions: basic declaration

--- main.go
+++ main.go
@@ -2,31 +2,15 @@ package main
 
 import "fmt"
 
-var (
-	name string
-	age  int
-)
-
 func main() {
-	var (
-		fullname   string
-		occupation string
-	)
+	sayHello("Davi Silva")
+	fmt.Println("Total:", sum(1, 2))
+}
 
-	fullname = "Davi Silva"
-	occupation = "Software Engineer"
+func sum(a, b int) int {
+	return a + b
+}
 
-	message := "Go 101 - lesson 03"
-	// var a, b, c int32             // multiples declaration
-	// var d, e, f = true, 2.3, "Hi" // multiples attribution
-
-	fmt.Println(message)
-	fmt.Println(fullname)
-	fmt.Println(occupation)
-
-	// swap values
-	var x, y = 10, 20
-	fmt.Println(x, y)
-	x, y = y, x
-	fmt.Println(x, y)
+func sayHello(name string) {
+	fmt.Println(name)
 }
