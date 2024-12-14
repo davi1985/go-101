5e03273877cfb8dbefc97ad9dd1112f58c00748d
Davi Silva <davisilvaphoto@gmail.com>
2024-12-14 14:53:23 -0300

    swap values and mutlples declaration and attribution

--- main.go
+++ main.go
@@ -17,8 +17,16 @@ func main() {
 	occupation = "Software Engineer"
 
 	message := "Go 101 - lesson 03"
+	// var a, b, c int32             // multiples declaration
+	// var d, e, f = true, 2.3, "Hi" // multiples attribution
 
 	fmt.Println(message)
 	fmt.Println(fullname)
 	fmt.Println(occupation)
+
+	// swap values
+	var x, y = 10, 20
+	fmt.Println(x, y)
+	x, y = y, x
+	fmt.Println(x, y)
 }
