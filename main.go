ec1ad06ba439e822de8eea722e9c75d558eda83d
Davi Silva <davisilvaphoto@gmail.com>
2024-12-14 17:07:01 -0300

    struct methods

--- main.go
+++ main.go
@@ -8,6 +8,23 @@ type Person struct {
 	Occupation string
 }
 
+func (p Person) String() string {
+	return fmt.Sprintf("I'm %s, I'm %d and I'm %s", p.Name, p.Age, p.Occupation)
+}
+
+type Category struct {
+	Name   string
+	Father *Category
+}
+
+func (c Category) HasParent() bool {
+	return c.Father != nil
+}
+
+func (c *Category) SetParent(parent *Category) {
+	c.Father = parent
+}
+
 func main() {
 	davi := Person{
 		Name:       "Davi Silva",
@@ -19,4 +36,15 @@ func main() {
 
 	fmt.Println(davi)
 	fmt.Println(joelma)
+
+	category := Category{Name: "Category 001"}
+
+	if !category.HasParent() {
+		fmt.Println("Category avoid")
+	}
+
+	category.SetParent(&Category{Name: "SubCategory"})
+
+	fmt.Println(category)
+
 }
