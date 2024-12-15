5c494274aeb2d716be713d0239f27ed258144a89
Davi Silva <davisilvaphoto@gmail.com>
2024-12-15 16:40:39 -0300

    inheritence

--- main.go
+++ main.go
@@ -3,48 +3,32 @@ package main
 import "fmt"
 
 type Person struct {
-	Name       string
-	Age        uint8
-	Occupation string
+	Name   string
+	Age    uint8
+	Status bool
 }
 
 func (p Person) String() string {
-	return fmt.Sprintf("I'm %s, I'm %d and I'm %s", p.Name, p.Age, p.Occupation)
+	return fmt.Sprintf("I'm %s, I'm %d", p.Name, p.Age)
 }
 
-type Category struct {
-	Name   string
-	Father *Category
+type NaturalPerson struct {
+	Person
+	lastName string
+	cpf      string
 }
 
-func (c Category) HasParent() bool {
-	return c.Father != nil
-}
-
-func (c *Category) SetParent(parent *Category) {
-	c.Father = parent
+type LegalEntity struct {
+	cnpj          string
+	corporateName string
 }
 
 func main() {
-	davi := Person{
-		Name:       "Davi Silva",
-		Occupation: "Software Engineer",
-		Age:        39,
+	person1 := NaturalPerson{
+		Person:   Person{Name: "Davi", Age: 39, Status: true},
+		lastName: "Silva",
+		cpf:      "001.002.003-44",
 	}
 
-	joelma := Person{"Joelma", 36, "Wife"}
-
-	fmt.Println(davi)
-	fmt.Println(joelma)
-
-	category := Category{Name: "Category 001"}
-
-	if !category.HasParent() {
-		fmt.Println("Category avoid")
-	}
-
-	category.SetParent(&Category{Name: "SubCategory"})
-
-	fmt.Println(category)
-
+	fmt.Println(person1)
 }
