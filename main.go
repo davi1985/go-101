91ea8fc17192a522cb427d108b9e3e0a28425626
Davi Silva <davisilvaphoto@gmail.com>
2024-12-24 13:44:30 -0300

    channels

--- main.go
+++ main.go
@@ -5,24 +5,33 @@ import (
 	"time"
 )
 
-func numbers() {
+func numbers(done chan<- bool) {
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%d \n", i)
+		fmt.Printf("%d ", i)
 		time.Sleep(time.Millisecond * 150)
 	}
+
+	done <- true
 }
 
-func letters() {
+func letters(done chan<- bool) {
 	for i := 'a'; i < 'l'; i++ {
-		fmt.Printf("%c \n", i)
+		fmt.Printf("%c ", i)
 		time.Sleep(time.Millisecond * 230)
 	}
+
+	done <- true
 }
 
 func main() {
-	go numbers()
-	go letters()
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
 
-	time.Sleep(3 * time.Second)
+	go numbers(ch1)
+	go letters(ch2)
+
+	<-ch1
+	<-ch2
+
 	fmt.Println("End")
 }
